77b5ad9d8b65f886c243f2b2707ebe90b55c0ce6
Joey Zhou <josephyzhou@gmail.com>
2015-08-28 17:49:28 -0700

    adding class

---
+++ JZAvatarNSImageView.swift
@@ -0,0 +1,62 @@
+//
+//  JZAvatarNSImageView.swift
+//
+//  Created by Joey Zhou on 8/28/15.
+//  Copyright (c) 2015 Joey Zhou. All rights reserved.
+//
+
+import Cocoa
+import AppKit
+
+class JZAvatarNSImageView: NSImageView {
+    
+    var cornerRadius: CGFloat = 0.0
+    var borderWidth: CGFloat = 1.0
+    var borderColor: NSColor
+    
+    override init(frame frameRect: NSRect) {
+        borderColor = NSColor.grayColor()
+        super.init(frame: frameRect)
+        cornerRadius = frame.height/2.0
+    }
+
+    required init?(coder: NSCoder) {
+        borderWidth = 1.0;
+        borderColor = NSColor.grayColor()
+        super.init(coder: coder)
+        cornerRadius = frame.height/2.0
+
+    }
+    
+    convenience init?(f: NSRect, bc: NSColor, bw: CGFloat) {
+        self.init(frame: f)
+        borderColor = bc
+        borderWidth = bw
+        redraw()
+    }
+    
+    convenience init?(f: NSRect, i: NSImage, bc: NSColor, bw: CGFloat) {
+        self.init(frame: f)
+        borderColor = bc
+        borderWidth = bw
+        image = i
+        redraw()
+    }
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+    }
+    
+    func redraw() {
+        if (frame.width != frame.height) {
+            println("Warning: Height and Width should be the same for image view");
+        }
+        
+        self.wantsLayer = true
+        layer?.borderWidth = borderWidth
+        layer?.borderColor = borderColor.CGColor
+        layer?.cornerRadius = cornerRadius
+
+    }
+    
+}
